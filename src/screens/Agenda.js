8a8e9c9416def669b523d3f888c5f5b7b554f0f1
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-23 10:44:19 -0300

    Add FlatList

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react"
-import { StyleSheet, Text, View, ImageBackground } from "react-native"
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  FlatList
+} from "react-native"
 import moment from "moment"
 import "moment/locale/pt-br"
 import todayImage from "../../assets/imgs/today.jpg"
@@ -7,6 +13,59 @@ import commonStyles from "../commonStyles"
 import Task from "../components/Task"
 
 export default class Agenda extends Component {
+  state = {
+    tasks: [
+      {
+        id: Math.random(),
+        description: "Comprar o curso React Native",
+        estimateAt: new Date(),
+        doneAt: new Date()
+      },
+      {
+        id: Math.random(),
+        description: "Concluir o curso React Native",
+        estimateAt: new Date((new Date).setMonth((new Date).getMonth() + 1)),
+        doneAt: null
+      },
+      {
+        id: Math.random(),
+        description: "Construir um app poquera",
+        estimateAt: new Date((new Date).setMonth((new Date).getMonth() + 3)),
+        doneAt: null
+      },
+      {
+        id: Math.random(),
+        description: "Ficar rico",
+        estimateAt: new Date((new Date).setMonth((new Date).getMonth() + 6)),
+        doneAt: null
+      },
+      {
+        id: Math.random(),
+        description: "Construir outro app poquera",
+        estimateAt: new Date((new Date).setMonth((new Date).getMonth() + 9)),
+        doneAt: null
+      },
+      {
+        id: Math.random(),
+        description: "Ficar mais rico ainda",
+        estimateAt: new Date((new Date).setMonth((new Date).getMonth() + 12)),
+        doneAt: null
+      },
+      {
+        id: Math.random(),
+        description: "Comprar uma casa beira rio",
+        estimateAt: new Date((new Date).setMonth((new Date).getMonth() + 15)),
+        doneAt: null
+      },
+      {
+        id: Math.random(),
+        description: "Se aponsentar!",
+        estimateAt: new Date((new Date).setMonth((new Date).getMonth() + 15)),
+        doneAt: null
+      },
+    ]
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -20,26 +79,9 @@ export default class Agenda extends Component {
           </View>
         </ImageBackground>
         <View style={styles.taskContainer}>
-          <Task description="Tarefa pendente"
-            estimateAt={new Date()} doneAt={null} />
-          <Task description="Tarefa concluída"
-            estimateAt={new Date()} doneAt={new Date()} />
-          <Task description="Tarefa pendente"
-            estimateAt={new Date()} doneAt={null} />
-          <Task description="Tarefa concluída"
-            estimateAt={new Date()} doneAt={new Date()} />
-          <Task description="Tarefa pendente"
-            estimateAt={new Date()} doneAt={null} />
-          <Task description="Tarefa concluída"
-            estimateAt={new Date()} doneAt={new Date()} />
-          <Task description="Tarefa pendente"
-            estimateAt={new Date()} doneAt={null} />
-          <Task description="Tarefa concluída"
-            estimateAt={new Date()} doneAt={new Date()} />
-          <Task description="Tarefa pendente"
-            estimateAt={new Date()} doneAt={null} />
-          <Task description="Tarefa concluída"
-            estimateAt={new Date()} doneAt={new Date()} />
+          <FlatList data={this.state.tasks}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) => <Task {...item} />} />
         </View>
       </View>
     )
